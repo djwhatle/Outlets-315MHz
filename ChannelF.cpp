2800c4233a3d7665881c2b206cb538c74e15b6e2
Derek Whatley <derekwhatley@gmail.com>
2016-08-21 20:53:06 -0400

    initial commit. working code but not within existing infrastructure

--- ChannelF.cpp
+++ ChannelF.cpp
@@ -2,7 +2,7 @@
 This is a library to control "Woods Wireless Remote Control Outlets"
 with a 315MHz RF Transmitter and Arduino
 
-You can pick up a set of outlets here:   
+You can pick up a set of outlets here:
         https://amzn.com/B003ZTWYXY
 Along with a transmitter to talk to it:
         http://amzn.com/B00LNADJS6
@@ -12,7 +12,7 @@ for more detailed information on this project
 
 Written by Cameron J. Webb  (cam@webbhacks.com)
 
-Copyright (c) 2015 Cameron J. Webb  
+Copyright (c) 2015 Cameron J. Webb
 MIT License, check LICENSE for more information
 All text above must be included in any redistribution
 
@@ -33,7 +33,7 @@ ChannelF::ChannelF(int pin) : Outlet(pin) {
 //      boolean state -> true=on ; false=off
 void ChannelF::toggle(int channel, bool state) {
     //  send signal 5x to cover for noise picked up in receiver
-    for (int i=0; i<5; i++) {   
+    for (int i=0; i<5; i++) {
         switch(channel) {
             case 1:
                 state ? ch1ON() : ch1OFF();
@@ -54,146 +54,406 @@ void ChannelF::toggle(int channel, bool state) {
 
 //  Ch.1  -  ON
 void ChannelF::ch1ON() {
-    preamble(); // initiate communication with receiver
-    
-    //  fluff
-    for (int i=0; i<2; i++) {
-        highShort();
-        lowLong();
-    }
-
-    // payload
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
     highLong();
     lowShort();
     highShort();
     lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
 
-    //  fluff
-    for (int i=0; i<6; i++) {
-        highShort();
-        lowLong();
-    }
+    lowLong();
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    // preamble(); // initiate communication with receiver
+    //
+    // //  fluff
+    // for (int i=0; i<2; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
+    //
+    // // payload
+    // highLong();
+    // lowShort();
+    // highShort();
+    // lowLong();
+    //
+    // //  fluff
+    // for (int i=0; i<6; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
 
     //  pause
-    delayMicroseconds(11800);
+    // delayMicroseconds(11800);
 }
 
 //  Ch.1  -  OFF
 void ChannelF::ch1OFF() {
-    preamble(); //  initiate communication with receiver
-
-    //  fluff
-    for (int i=0; i<2; i++) {
-        highShort();
-        lowLong();
-    }
-
-    //  payload
+    // preamble(); //  initiate communication with receiver
+    //
+    // //  fluff
+    // for (int i=0; i<2; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
+    //
+    // //  payload
+    // highShort();
+    // lowLong();
+    // highLong();
+    // lowShort();
+    //
+    // //  fluff
+    // for (int i=0; i<6; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
     highShort();
     lowLong();
     highLong();
     lowShort();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
 
-    //  fluff
-    for (int i=0; i<6; i++) {
-        highShort();
-        lowLong();
-    }
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
 
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
     //  pause
-    delayMicroseconds(11800);
+    // delayMicroseconds(11800);
 }
 
 //  Ch.2  -  ON
 void ChannelF::ch2ON() {
-    preamble(); //  initiate communication with receiver
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
 
-    //  fluff
-    for (int i=0; i<4; i++) {
-        highShort();
-        lowLong();
-    }
-
-    //  payload
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
     highLong();
     lowShort();
     highShort();
     lowLong();
 
-    //  fluff
-    for (int i=0; i<4; i++) {
-        highShort();
-        lowLong();
-    }
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
 
-    //  pause
-    delayMicroseconds(11800);
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    // preamble(); //  initiate communication with receiver
+    //
+    // //  fluff
+    // for (int i=0; i<4; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
+    //
+    // //  payload
+    // highLong();
+    // lowShort();
+    // highShort();
+    // lowLong();
+    //
+    // //  fluff
+    // for (int i=0; i<4; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
+    //
+    // //  pause
+    // delayMicroseconds(11800);
 }
 
 //  Ch.2  -  OFF
 void ChannelF::ch2OFF() {
-    preamble(); //  initiate communication with receiver
+    // preamble(); //  initiate communication with receiver
+    //
+    // //  fluff
+    // for (int i=0; i<4; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
+    //
+    // //  payload
+    // // highShort();
+    // // lowLong();
+    // // highLong();
+    // // lowShort();
+    // highShort();
+    // lowLong();
+    // highShort();
+    // lowLong();
+    //
+    // highShort(); //hs
+    // lowLong(); //ll
+    // highLong(); //hl
+    // lowShort(); //ls
+    // highShort(); //hs
+    // lowLong(); //ll
+    // highShort(); //hs
+    // lowLong(); //ll
+    // highShort(); //hs
+    // lowShort(); //ls
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
 
-    //  fluff
-    for (int i=0; i<4; i++) {
-        highShort();
-        lowLong();
-    }
-
-    //  payload
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
     highShort();
     lowLong();
     highLong();
     lowShort();
 
-    //  fluff
-    for (int i=0; i<4; i++) {
-        highShort();
-        lowLong();
-    }
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+
+    // //  fluff
+    // for (int i=0; i<4; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
 
     //  pause
-    delayMicroseconds(11800);
+    // delayMicroseconds(11800);
 }
 
 //  Ch.3  -  ON
 void ChannelF::ch3ON() {
-    preamble(); //  initiate communication with receiver
+    // preamble(); //  initiate communication with receiver
 
-    //  payload
     highShort();
     lowLong();
     highLong();
     lowShort();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
 
-    //  fluff
-    for (int i=0; i<8; i++) {
-        highShort();
-        lowLong();
-    }
+    lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
 
-    //  pause
-    delayMicroseconds(11800);
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+
+    // //  payload
+    // highShort();
+    // lowLong();
+    // highLong();
+    // lowShort();
+    //
+    // //  fluff
+    // for (int i=0; i<8; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
+    //
+    // //  pause
+    // delayMicroseconds(11800);
 }
 
 //  Ch.3  -  OFF
 void ChannelF::ch3OFF() {
-    preamble(); //  initiate communication with receiver
-
-    //  payload
+    highShort();
+    lowLong();
+    highLong();
+    lowShort();
     highLong();
     lowShort();
     highShort();
     lowLong();
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
 
-    //  fluff
-    for (int i=0; i<8; i++) {
-        highShort();
-        lowLong();
-    }
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
 
-    //  pause
-    delayMicroseconds(11800);
+    highLong();
+    lowShort();
+    highShort();
+    lowLong();
+    highShort();
+    lowLong();
+    highShort();
+
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    lowLong();
+    // preamble(); //  initiate communication with receiver
+    //
+    // //  payload
+    // highLong();
+    // lowShort();
+    // highShort();
+    // lowLong();
+    //
+    // //  fluff
+    // for (int i=0; i<8; i++) {
+    //     highShort();
+    //     lowLong();
+    // }
+    //
+    // //  pause
+    // delayMicroseconds(11800);
 }
-
-
